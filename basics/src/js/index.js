d8a5ea4d8fb876d45348334b2951c86f53ca2d04
janarthan <janarthan@finnovation.lk>
2018-05-09 13:45:43 +0530

    Use v-bind

--- basics/src/js/index.js
+++ basics/src/js/index.js
@@ -4,3 +4,10 @@ new Vue({
         message: 'Hey Vue.js!'
     }
 });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
