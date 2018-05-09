2a30ca430075d3fb82b459cf2829e200fe2e25dd
janarthan <janarthan@finnovation.lk>
2018-05-09 18:39:37 +0530

    Refactor

--- basics/src/js/index.js
+++ basics/src/js/index.js
@@ -3,20 +3,25 @@ Vue.component('todo-item', {
     template: '<li>{{ todo.text }}</li>'
 });
 
+var data = {};
+var methods = {};
+
+data.message = 'Hey! Vue.js!';
+data.seen = true;
+data.todos = [
+    {text: 'Learn js'},
+    {text: 'Learn angular'},
+    {text: 'Learn Vue'},
+    {text: 'Build something awesome'}
+];
+
+methods.reverseMessage = function () {
+    this.message = this.message.split('').reverse().join('')
+};
+
+//vue instance
 var app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hey! Vue.js!',
-        seen: true,
-        todos: [
-            { text: 'Learn js' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-        ]
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
+    data: data,
+    methods: methods
 });
