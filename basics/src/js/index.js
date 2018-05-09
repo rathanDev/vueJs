adf066d66081a7bb9c8c20a957e7095ea9bfbbd0
janarthan <janarthan@finnovation.lk>
2018-05-09 18:06:31 +0530

    Refactor

--- basics/src/js/index.js
+++ basics/src/js/index.js
@@ -1,31 +1,17 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hey Vue.js!'
-    }
-});
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-});
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-});
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
+        message: 'Hey! Vue.js!',
+        seen: true,
         todos: [
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
         ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 });
