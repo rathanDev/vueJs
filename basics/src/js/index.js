3b3d5cdd9969adfc30dcaedab07d381ef2ed9e06
janarthan <janarthan@finnovation.lk>
2018-05-09 18:27:40 +0530

    Use template

--- basics/src/js/index.js
+++ basics/src/js/index.js
@@ -1,10 +1,15 @@
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Hey! Vue.js!',
         seen: true,
         todos: [
-            { text: 'Learn JavaScript' },
+            { text: 'Learn js' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
         ]
